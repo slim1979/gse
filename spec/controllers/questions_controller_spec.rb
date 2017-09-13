a4d0dce51d1b4ae592be1e1aa4af7b8758e4abd7
slim1979 <chinezan@yandex.ru>
2017-09-13 18:58:53 +0300

    refactoring and post #create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     # the better way to assign variable instead of @questions = FactoryGirl.create_list etc...
     let(:questions) {
@@ -16,8 +18,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
-
     before { get :show, params: { id: question } }
 
     it 'assign request question to @question' do
@@ -39,8 +39,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
-    let(:question) { create(:question) }
-
     before { get :edit, params: { id: question } }
 
     it 'assign request question to @question' do
@@ -49,6 +47,27 @@ RSpec.describe QuestionsController, type: :controller do
     it 'renders edit template' do
       expect(response).to render_template :edit
     end
+  end
 
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves new question to DB' do
+        # attributes_for returns a hash of attributes from factory girl
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+      it 'redirect to show' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+    context 'with invalid attributes' do
+      it 'does not save the new question to DB' do
+        expect { post :create, params: { question: attributes_for(:invalid_question) } }.to_not change(Question, :count)
+      end
+      it 're-renders new template' do
+        post :create, params: { question: attributes_for(:invalid_question) }
+        expect(response).to render_template :new
+      end
+    end
   end
 end
