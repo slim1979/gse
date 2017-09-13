da437009ab88a816b9fc1d06d6897ff09af0f625
slim1979 <chinezan@yandex.ru>
2017-09-13 18:12:40 +0300

    rspec method show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,18 +2,29 @@ require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
+    # the better way to assign variable instead of @questions = FactoryGirl.create_list etc...
+    let(:questions) {
+      create_list(:question, 2) # FactoryGirl. no longer needed, since we add config.include FactoryGirl::Syntax::Methods in rails_helper.rb
+    }
+    before { get :index }
     it 'populates an array of all questions' do
-      question1 = FactoryGirl.create(:question)
-      question2 = FactoryGirl.create(:question)
-
-      get :index
-
-      expect(assigns(:questions)).to match_array([question1, question2])
+      expect(assigns(:questions)).to match_array(questions)
     end
-
     it 'renders index view' do
-      get :index
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, params: { id: question } }
+
+    it 'assign request question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+    it 'renders show template' do
+      expect(response).to render_template :show
     end
   end
 end
