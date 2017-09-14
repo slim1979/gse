bc2f53d6df1d6ddfddd18006a6d869c691fb78cb
slim1979 <chinezan@yandex.ru>
2017-09-14 22:21:40 +0300

    refactoring

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,32 +5,36 @@ RSpec.describe AnswersController, type: :controller do
   let(:answer) { create(:answer, question: question) }
 
   describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
     it 'assign a new question to @question' do
-      get :new, params: { question_id: question }
       expect(assigns(:answer)).to be_a_new(Answer)
     end
     it 'renders new view' do
-      get :new, params: { question_id: question }
       expect(response).to render_template :new
     end
   end
 
   describe 'POST #create' do
+    let(:valid_post_create) { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
+    let(:invalid_post_create) { post :create, params: { question_id: question, answer: { body: nil } } }
+
     context 'with valid attributes' do
       it 'saves answer to database' do
-        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+        expect { valid_post_create }.to change(Answer, :count).by(1)
       end
       it 'redirect to show' do
-        post :create, params: { question_id: question, answer: attributes_for(:answer) }
+        valid_post_create
         expect(response).to redirect_to answer_path(assigns(:answer))
       end
     end
+
     context 'with invalid attributes' do
       it 'does not save answer to db' do
-        expect { post :create, params: { question_id: question, answer: { body: nil } } }.to_not change(Answer, :count)
+        expect { invalid_post_create }.to_not change(Answer, :count)
       end
       it 're-render template new' do
-        post :create, params: { question_id: question, answer: { body: nil } }
+        invalid_post_create
         expect(response).to render_template :new
       end
     end
