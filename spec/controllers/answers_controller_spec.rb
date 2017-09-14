0bd55372f4c8cf1990b58f4e658582e917b5d582
slim1979 <chinezan@yandex.ru>
2017-09-14 21:29:07 +0300

    improved

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe AnswersController, type: :controller do
     let(:answer) { create(:answer, question: question) }
     context 'with valid attributes' do
       it 'saves answer to database' do
-        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
       end
       it 'redirect to show' do
-        post :create, params: { question_id: question.id, answer: attributes_for(:answer) }
+        post :create, params: { question_id: question, answer: attributes_for(:answer) }
         expect(response).to redirect_to answer_path(assigns(:answer))
       end
     end
     context 'with invalid attributes' do
       it 'does not save answer to db' do
-        expect { post :create, params: { question_id: question.id, answer: { body: nil } } }.to_not change(Answer, :count)
+        expect { post :create, params: { question_id: question, answer: { body: nil } } }.to_not change(Answer, :count)
       end
       it 're-render template new' do
-        post :create, params: { question_id: question.id, answer: { body: nil } }
+        post :create, params: { question_id: question, answer: { body: nil } }
         expect(response).to render_template :new
       end
     end
