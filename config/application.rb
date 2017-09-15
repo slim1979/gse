9d4f2316fa4ab0a4e75a7bd854d7d66ee3ac936f
slim1979 <chinezan@yandex.ru>
2017-09-15 17:37:04 +0300

    syntax error fixed

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Gse
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
-      g.tets_framework :rspec,
+      g.test_framework :rspec,
                        fixtures: true,
                        view_specs: false,
                        helper_specs: false,
