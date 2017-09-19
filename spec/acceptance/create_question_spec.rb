999c05b975d9a1aaaf1233b410f1a35a6fc4c54b
slim1979 <chinezan@yandex.ru>
2017-09-20 02:15:46 +0300

    2 features fullfilled

---
+++ spec/acceptance/create_question_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'Create question', %q(
+  In order to get answer from community
+  As an authenticated user
+  I want to be able to ask the question
+) do
+
+  scenario 'Authenticated user created the question' do
+    User.create(email: 'user@test.com', password: '12345678')
+
+    visit new_user_session_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    visit questions_path
+    click_on 'Ask question'
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text'
+    click_on 'Create'
+
+    expect(page).to have_content 'Your question created successfully!'
+  end
+
+  scenario 'Authenticated user created the question' do
+
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
