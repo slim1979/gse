c49de62a82ed0c547430056880d2cd1a83370ef1
slim1979 <chinezan@yandex.ru>
2017-09-21 02:20:22 +0300

    user can delete only it own answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,6 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_question
+  before_action :set_question, except: :destroy
 
   def new
     @answer = @question.answers.new
@@ -14,6 +14,19 @@ class AnswersController < ApplicationController
       redirect_to @question
     else
       render :new
+    end
+  end
+
+  def destroy
+    @answer = Answer.find(params[:id])
+    @question = @answer.question
+    if @answer.user == current_user
+      @answer.destroy
+      redirect_to @question
+      flash[:notice] = 'Your answer successfully deleted!'
+    else
+      redirect_to @question
+      flash[:notice] = 'You can delete only your own content'
     end
   end
 
---
+++ spec/acceptance/user/destroy_answers_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+feature 'Destroy answer', %q(
+  In order to be able to refuse to answer
+  As an author
+  I want to be able to delete own answer
+) do
+
+  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
+  given(:question) { create(:question, user: user) }
+  given(:answer) { create(:answer, question: question, user: user) }
+
+  before do
+    question
+    answer
+  end
+
+  scenario 'Authenticated user tries to delete his own answer' do
+
+    sign_in(user)
+    visit question_path(question)
+    click_on 'Delete answer'
+
+    expect(page).to have_content 'Your answer successfully deleted!'
+  end
+
+  scenario 'Authenticated user tries to delete someone else\'s answer' do
+    sign_in(user2)
+    visit question_path(question)
+
+    click_on 'Delete answer'
+    expect(page).to have_content 'You can delete only your own content'
+  end
+
+  scenario 'Unauthenticated user tries to delete someone else\'s answer' do
+    visit question_path(question)
+    click_on 'Delete answer'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
