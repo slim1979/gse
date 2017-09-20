515b94e1c2b57ef49f6d86d1862f358aa70f7d27
slim1979 <chinezan@yandex.ru>
2017-09-21 00:27:32 +0300

    name changed

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -1,4 +1,4 @@
-module AcceptanceMacros
+module AcceptanceHelper
   def sign_in(user)
     visit new_user_session_path
     fill_in 'Email', with: user.email
@@ -13,6 +13,25 @@ module AcceptanceMacros
     questions.each do |question|
       expect(page).to have_content question.title
       expect(page).to have_content question.body
+    end
+  end
+
+  def expectations
+    expect(page).to have_content question1.title
+    expect(page).to have_content question1.body
+    answers1.each do |answer|
+      expect(page).to have_content answer.body
+    end
+    expect(page).to have_content question2.title
+    expect(page).to have_content question2.body
+    answers2.each do |answer|
+      expect(page).to have_content answer.body
+    end
+
+    expect(page).to have_content question3.title
+    expect(page).to have_content question3.body
+    answers3.each do |answer|
+      expect(page).to have_content answer.body
     end
   end
 end
