5fecc1640ae28be2feabdf3237dc121d41d9037f
slim1979 <chinezan@yandex.ru>
2017-09-20 18:11:58 +0300

    factory makes more different questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,15 @@
 FactoryGirl.define do
+  sequence :title do |n|
+    "Question Title №#{n}"
+  end
+
+  sequence :body do
+    ('a'..'z').to_a.sample(51).join
+  end
+
   factory :question do
-    title 'MyString'
-    body 'MyText'
+    title
+    body
   end
 
   factory :invalid_question, class: 'Question' do
