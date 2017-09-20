b1f37a8beb5a68bf2e8a81ee96884f8e84230c2e
slim1979 <chinezan@yandex.ru>
2017-09-20 18:13:40 +0300

    user now can answer the question)

--- spec/acceptance/questions/answer_the_question_spec.rb
+++ spec/acceptance/questions/answer_the_question_spec.rb
@@ -9,26 +9,28 @@ feature 'Answer the question', %q(
   given(:user) { create(:user) }
   given(:question) { create(:question) }
 
-  scenario 'Authenticated user created the question' do
+  scenario 'Authenticated user can answer the question' do
 
     sign_in(user)
     question
 
     visit questions_path
     click_on 'Show question'
-
-    click_on 'Answer the question'
     fill_in 'Body', with: 'Some text to solve problem'
-    click_on 'Answer'
+    click_on 'Answer the question'
 
-    expect(page).to have_content 'Some text to solve problem'  
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+    expect(page).to have_content 'Some text to solve problem'
   end
 
-  scenario 'Authenticated user created the question' do
+  scenario 'Unauthenticated user can\'t answer the question' do
 
+    question
     visit questions_path
-    click_on 'Ask question'
+    click_on 'Show question'
 
+    click_on 'Answer the question'
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
 end
