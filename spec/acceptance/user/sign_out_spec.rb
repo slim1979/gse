fe018d6885435b31caff9bce08b19c05ea13c14a
slim1979 <chinezan@yandex.ru>
2017-09-21 01:09:50 +0300

    authenticated user can sign out

---
+++ spec/acceptance/user/sign_out_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User sign out', %q(
+  In order to be able close session
+  As an user
+  I want to be able to sign out
+) do
+
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user tried to sign out' do
+
+    sign_in(user)
+    click_on 'Выйти'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+
+  scenario 'Unregistered user tried to sign out' do
+    visit root_path
+    expect(page).to_not have_content 'Выйти'
+  end
+end
