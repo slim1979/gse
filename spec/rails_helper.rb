14b8e9e6e0b3a1fcc081607f0b860919a5461406
slim1979 <chinezan@yandex.ru>
2017-09-21 00:27:46 +0300

    module created and extended

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,8 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.extend ControllerMacros, type: :controller
-  config.include AcceptanceMacros, type: :feature
+  config.include AcceptanceHelper, type: :feature
+  config.extend AcceptanceMacros, type: :feature
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
