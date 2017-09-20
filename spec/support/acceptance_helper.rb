cfddbefbfdd44c508144a2f63dc9614258b08378
slim1979 <chinezan@yandex.ru>
2017-09-20 15:08:07 +0300

    refactoring

--- spec/support/acceptance_helper.rb
+++ spec/support/acceptance_helper.rb
@@ -5,4 +5,14 @@ module AcceptanceMacros
     fill_in 'Password', with: user.password
     click_on 'Log in'
   end
+
+  def reaching_the_questions_list
+    questions
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
 end
