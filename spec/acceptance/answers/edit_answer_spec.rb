4e3c50a1141b0f4d3c5e21d75512ded94244c88d
slim1979 <chinezan@yandex.ru>
2017-09-30 01:11:39 +0300

    spec init

---
+++ spec/acceptance/answers/edit_answer_spec.rb
@@ -0,0 +1,45 @@
+require 'acceptance_helper'
+
+feature 'Answer editing', %q{
+  In order to fix mistake
+  As an author of Answer
+  I qant to be able to edit answer
+} do
+
+  given(:user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given!(:answer) { create(:answer, user: user, question: question) }
+
+  scenario 'Unauthenticated user tried to edit answer' do
+    visit question_path(question)
+
+    expect(page).to_not have_link 'edit answer'
+  end
+
+  describe 'Authenticated user' do
+    before do
+      sign_in user
+      visit question_path(question)
+    end
+
+    scenario 'see link to edit his answer' do
+      within '.edit' do
+        expect(page).to have_link 'Edit answer'
+      end
+    end
+
+    scenario 'tried to edit his answer', js: true do 
+      within '.answer' do
+        click_on 'Edit answer'
+        fill_in 'Answer', with: 'some new answer'
+        click_on 'Save'
+
+        expect(page).to_not have_content answer.body
+        expect(page).to have_content 'some new answer'
+        expect(page).to_not have_selector 'textarea'
+      end
+    end
+
+    scenario 'tried to edit someone else answer'
+  end
+end
