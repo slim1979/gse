bf07d83ad2d60e2f07f2a97c9507cad2fbebfdf6
slim1979 <chinezan@yandex.ru>
2017-09-30 01:12:51 +0300

    action update

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,7 @@
 class AnswersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_question, except: :destroy
+  before_action :set_question, only: :create
+  before_action :set_answer, except: :create
 
   def create
     @answer = @question.answers.new(answer_params)
@@ -9,8 +10,12 @@ class AnswersController < ApplicationController
     @answer.save
   end
 
+  def update
+    @answer.update(answer_params)
+    @question = @answer.question
+  end
+
   def destroy
-    @answer = Answer.find(params[:id])
     @question = @answer.question
     redirect_to @question
 
@@ -26,6 +31,10 @@ class AnswersController < ApplicationController
 
   def set_question
     @question = Question.find(params[:question_id])
+  end
+
+  def set_answer
+    @answer = Answer.find(params[:id])
   end
 
   def answer_params
