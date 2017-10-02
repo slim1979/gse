553f168a170b4b618379a3f44cb9b9464aae7442
slim1979 <chinezan@yandex.ru>
2017-10-02 17:13:40 +0300

    best answer assign logic

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,12 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
+
+  def turn_off_old_best_answer
+    answers.find(best_answer).update(best_answer: false) if best_answer
+  end
+
+  def turn_on_new_best_answer
+    answers.find(best_answer).update(best_answer: true) if best_answer
+  end
 end
