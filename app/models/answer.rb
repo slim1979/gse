40b243a53618dfa2a3a8364effe43a8a754161a7
slim1979 <chinezan@yandex.ru>
2017-10-03 15:54:45 +0300

    best answer switch method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,11 @@ class Answer < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  def best_answer_switch
+    transaction do
+      question.answers.where(best_answer: true).update_all(best_answer: false)
+      update(best_answer: true)
+    end
+  end
 end
