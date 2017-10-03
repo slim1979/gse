fc41ce604ebf6d630f8e9ca6ea2c8434e7869d72
slim1979 <chinezan@yandex.ru>
2017-10-03 23:19:18 +0300

    unite 3 context to one describe, test for img show for best answer

--- spec/acceptance/answers/best_answer_spec.rb
+++ spec/acceptance/answers/best_answer_spec.rb
@@ -14,52 +14,54 @@ feature 'Choose best answer', %q{
   given!(:answer2) { create(:answer, user: user2, question: question) }
   given!(:answer3) { create(:answer, user: user3, question: question) }
 
-  scenario 'Authenticated user sees link to best answer', js: true do
-    sign_in user
-    visit question_path(question)
-    expect(page).to have_link 'Best answer'
-  end
+  describe 'Author of question' do
 
-  scenario 'Author of question choosed the best answer', js: true do
-    sign_in user
-    visit question_path(question)
-
-    within '#answer_3' do
-      click_on 'Best answer'
+    before do
+      sign_in user
+      visit question_path(question)
     end
 
-    within '.answer:first-child' do
-      expect(page).to_not have_content answer1.body
-      expect(page).to_not have_content answer2.body
-      expect(page).to have_content answer3.body
-      expect(page).to_not have_link 'Best answer'
-    end
-    # answers = all('.answer')
-    # expect(answers.first).to ....
-  end
-
-  scenario 'Author of question choosed another best answer', js: true do
-    sign_in user
-    visit question_path(question)
-
-    within '#answer_3' do
-      click_on 'Best answer'
-    end
-
-    within '#answer_2' do
-      click_on 'Best answer'
-    end
-
-    within '.answer:first-child' do
-      expect(page).to_not have_content answer1.body
-      expect(page).to_not have_content answer3.body
-      expect(page).to have_content answer2.body
-      expect(page).to_not have_link 'Best answer'
-    end
-
-    within "#answer_#{answer3.id}" do
-      expect(page).to have_content answer3.body
+    scenario 'sees link to best answer', js: true do
       expect(page).to have_link 'Best answer'
+    end
+
+    scenario 'choosed the best answer', js: true do
+      within '#answer_3' do
+        click_on 'Best answer'
+      end
+
+      within '.answer:first-child' do
+        expect(page).to_not have_content answer1.body
+        expect(page).to_not have_content answer2.body
+        expect(page).to have_content answer3.body
+        expect(page).to have_xpath('//img[@src="/assets/best-272f2f9e50eba3e954cc7a3de060a1d47823677f3589b08e0aa270c327d02782.png"]')
+        expect(page).to_not have_link 'Best answer'
+      end
+      # answers = all('.answer')
+      # expect(answers.first).to ....
+    end
+
+    scenario 'choosed another best answer', js: true do
+      within '#answer_3' do
+        click_on 'Best answer'
+      end
+
+      within '#answer_2' do
+        click_on 'Best answer'
+      end
+
+      within '.answer:first-child' do
+        expect(page).to_not have_content answer1.body
+        expect(page).to_not have_content answer3.body
+        expect(page).to have_content answer2.body
+        expect(page).to have_xpath('//img[@src="/assets/best-272f2f9e50eba3e954cc7a3de060a1d47823677f3589b08e0aa270c327d02782.png"]')
+        expect(page).to_not have_link 'Best answer'
+      end
+
+      within "#answer_#{answer3.id}" do
+        expect(page).to have_content answer3.body
+        expect(page).to have_link 'Best answer'
+      end
     end
   end
 
