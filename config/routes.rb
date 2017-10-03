a36dd41b98aa0f25c475504286b45941d2be43c9
slim1979 <chinezan@yandex.ru>
2017-10-03 14:00:56 +0300

    best answer assign moved to answer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :questions do
-    resources :answers, shallow: true
-    patch :best_answer_assign, on: :member
+    resources :answers, shallow: true do
+      patch :best_answer_assign, on: :member
+    end
   end
   root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
