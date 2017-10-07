1d53f23eebf541948c080731100e8ef589e47a3f
slim1979 <chinezan@yandex.ru>
2017-10-08 02:40:39 +0300

    add nested form to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,8 +1,11 @@
 class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
+  has_many :attaches
 
   validates :body, presence: true
+
+  accepts_nested_attributes_for :attaches
 
   def best_answer_switch
     transaction do
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
+  it { should have_many :attaches }
   it { should validate_presence_of :body }
+  it { should accept_nested_attributes_for :attaches }
 end
