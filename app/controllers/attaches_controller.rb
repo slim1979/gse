4e2a62277fdeee45bb12eadc32d730bd285b795a
slim1979 <chinezan@yandex.ru>
2017-11-17 21:16:30 +0300

    respond in json on delete

--- app/controllers/attaches_controller.rb
+++ app/controllers/attaches_controller.rb
@@ -3,5 +3,9 @@ class AttachesController < ApplicationController
   def destroy
     @attach = Attach.find(params[:id])
     @attach.destroy if current_user.author_of?(@attach.attachable)
+
+    respond_to do |format|
+      format.json { render json: @attach }
+    end
   end
 end
