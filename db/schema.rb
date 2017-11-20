066fa13eb724ce760c585ccd551174818fd6a7dd
slim1979 <chinezan@yandex.ru>
2017-11-20 23:44:33 +0300

    vote model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007234446) do
+ActiveRecord::Schema.define(version: 20171120122341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,17 @@ ActiveRecord::Schema.define(version: 20171007234446) do
     t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "subject_id"
+    t.string "subject_type"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subject_id"], name: "index_votes_on_subject_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
---
+++ spec/factories/votes.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :vote do
+    user nil
+    subject nil
+    subject_type "MyString"
+    value ""
+  end
+end
