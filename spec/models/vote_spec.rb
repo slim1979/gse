54190a5d8672d41faceea84e4c5de36e51dcc50e
slim1979 <chinezan@yandex.ru>
2017-11-20 23:52:57 +0300

    model spec in 5 tests is green

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :user }
+  it { should belong_to :subject }
+  it { should validate_presence_of :user }
+  it { should validate_presence_of :subject }
+  it { should validate_presence_of :value }
 end
