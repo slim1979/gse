594aaea408ff0c2a59dd9a4034e61701c1631496
slim1979 <chinezan@yandex.ru>
2017-11-23 12:52:04 +0300

    val - user uniqueness, methods: update value, update subject votes count

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,17 @@ class Vote < ApplicationRecord
   belongs_to :subject, polymorphic: true
 
   validates :user, :subject, :value, presence: true
+  validates :user, uniqueness: { scope: :subject_id }
+
+  def update_value(vote_value)
+    unless value == vote_value
+      update(value: value + vote_value)
+      update_subject_votes_count(vote_value)
+    end
+  end
+
+  def update_subject_votes_count(vote_value)
+    new_value = subject.votes_count + vote_value
+    subject.update(votes_count: new_value)
+  end
 end
