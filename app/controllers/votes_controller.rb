9d3d2f78906df989e25d208d4f083f414795ac96
slim1979 <chinezan@yandex.ru>
2017-11-23 12:50:52 +0300

    action 'any'

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,17 @@
 class VotesController < ApplicationController
+
+  def any
+    subject_type = params[:subject_type]
+    subject_id = params[:subject_id]
+    votes_value = params[:votes_value].to_i
+
+    @vote = Vote.where(user: current_user, subject_id: subject_id, subject_type: subject_type).first
+    if @vote
+      @vote.update_value(votes_value)
+    else
+      @vote = Vote.create(user: current_user, subject_id: subject_id, subject_type: subject_type, value: votes_value)
+      @vote.update_subject_votes_count(votes_value)
+    end
+    respond_to { |format| format.json { render partial: 'votes/votes' } }
+  end
 end
