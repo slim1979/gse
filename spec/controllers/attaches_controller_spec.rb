a354395c88b3a9895ca399189c8c3d3a26bed13a
slim1979 <chinezan@yandex.ru>
2017-11-28 20:54:53 +0300

    format changed js => json

--- spec/controllers/attaches_controller_spec.rb
+++ spec/controllers/attaches_controller_spec.rb
@@ -13,29 +13,25 @@ RSpec.describe AttachesController, type: :controller do
       context 'as an attachable author' do
         before { sign_in user }
         it 'assigns request attach to @attach' do
-          delete :destroy, params: { id: question_attach }, format: :js
+          delete :destroy, params: { id: question_attach }, format: :json
           expect(assigns(:attach)).to eq question_attach
         end
         it 'reduce attaches count' do
           question_attach
-          expect { delete :destroy, params: { id: question_attach }, format: :js }.to change(question.attaches, :count).by(-1)
-        end
-        it 'render template destroy' do
-          delete :destroy, params: { id: question_attach }, format: :js
-          expect(response).to render_template :destroy
+          expect { delete :destroy, params: { id: question_attach }, format: :json }.to change(question.attaches, :count).by(-1)
         end
       end
       context 'as another attachable author' do
         it 'will not reduce attaches count' do
           sign_in user2
           question_attach
-          expect { delete :destroy, params: { id: question_attach }, format: :js }.to_not change(Attach, :count)
+          expect { delete :destroy, params: { id: question_attach }, format: :json }.to_not change(Attach, :count)
         end
       end
       context 'as unauthenticated user' do
         it 'will not reduce attaches count' do
           question_attach
-          expect { delete :destroy, params: { id: question_attach }, format: :js }.to_not change(Attach, :count)
+          expect { delete :destroy, params: { id: question_attach }, format: :json }.to_not change(Attach, :count)
         end
       end
     end
@@ -43,29 +39,25 @@ RSpec.describe AttachesController, type: :controller do
       context 'as an attachable author' do
         before { sign_in user }
         it 'assigns request attach to @attach' do
-          delete :destroy, params: { id: answer_attach }, format: :js
+          delete :destroy, params: { id: answer_attach }, format: :json
           expect(assigns(:attach)).to eq answer_attach
         end
         it 'reduce attaches count' do
           answer_attach
-          expect { delete :destroy, params: { id: answer_attach }, format: :js }.to change(answer.attaches, :count).by(-1)
-        end
-        it 'render template destroy' do
-          delete :destroy, params: { id: answer_attach }, format: :js
-          expect(response).to render_template :destroy
+          expect { delete :destroy, params: { id: answer_attach }, format: :json }.to change(answer.attaches, :count).by(-1)
         end
       end
       context 'as another attachable author' do
         it 'will not reduce attaches count' do
           sign_in user2
           answer_attach
-          expect { delete :destroy, params: { id: answer_attach }, format: :js }.to_not change(Attach, :count)
+          expect { delete :destroy, params: { id: answer_attach }, format: :json }.to_not change(Attach, :count)
         end
       end
       context 'as unauthenticated user' do
         it 'will not reduce attaches count' do
           answer_attach
-          expect { delete :destroy, params: { id: answer_attach }, format: :js }.to_not change(Attach, :count)
+          expect { delete :destroy, params: { id: answer_attach }, format: :json }.to_not change(Attach, :count)
         end
       end
     end
