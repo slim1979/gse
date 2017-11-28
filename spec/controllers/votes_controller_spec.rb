0dc40463ff5c715038bfc5cbfc5f248a0d3d38cd
slim1979 <chinezan@yandex.ru>
2017-11-28 20:52:28 +0300

    the only action 'any' tests is green

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
 
+  let(:user)      { create(:user) }
+  let(:user2)     { create(:user) }
+  let(:question)  { create(:question, user: user) }
+  let!(:answer)   { create(:answer, user: user2, question: question) }
+
+  describe 'PATCH#any' do
+    let(:vote_like_for_answer) { patch :any, params: { subject_type: Answer, subject_id: answer.id, votes_value: 1 }, format: :json }
+    let(:vote_dislike_for_answer) { patch :any, params: { subject_type: Answer, subject_id: answer.id, votes_value: -1 }, format: :json }
+
+    it 'vote \'like\' for answer will increase its rating' do
+      sign_in user
+      expect {
+        vote_like_for_answer
+        answer.reload
+      }.to change(answer, :votes_count).by(1)
+    end
+
+    it 'vote \'dislike\' for answer will decrease its rating' do
+      sign_in user
+      expect {
+        vote_dislike_for_answer
+        answer.reload
+      }.to change(answer, :votes_count).by(-1)
+    end
+  end
 end
