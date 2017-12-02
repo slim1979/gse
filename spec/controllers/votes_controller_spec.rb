376ba396b1d9353a3ef37e478093941c771e02fb
slim1979 <chinezan@yandex.ru>
2017-12-02 16:33:34 +0300

    question tests added

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe VotesController, type: :controller do
 
-  let(:user)      { create(:user) }
-  let(:user2)     { create(:user) }
-  let(:question)  { create(:question, user: user) }
+  let!(:user)      { create(:user) }
+  let!(:user2)     { create(:user) }
+  let!(:question) { create(:question, user: user) }
   let!(:answer)   { create(:answer, user: user2, question: question) }
 
-  describe 'PATCH#any' do
-    let(:vote_like_for_answer) { patch :any, params: { subject_type: Answer, subject_id: answer.id, votes_value: 1 }, format: :json }
-    let(:vote_dislike_for_answer) { patch :any, params: { subject_type: Answer, subject_id: answer.id, votes_value: -1 }, format: :json }
+  describe 'POST#create' do
+    let(:vote_like_for_answer) { post :create, params: { answer_id: answer, votes_value: 1 }, format: :json }
+    let(:vote_dislike_for_answer) { post :create, params: { answer_id: answer, votes_value: -1 }, format: :json }
+    let(:vote_like_for_question) { post :create, params: { question_id: question, votes_value: 1 }, format: :json }
+    let(:vote_dislike_for_question) { post :create, params: { question_id: question, votes_value: -1 }, format: :json }
 
     it 'vote \'like\' for answer will increase its rating' do
       sign_in user
@@ -25,6 +27,22 @@ RSpec.describe VotesController, type: :controller do
         vote_dislike_for_answer
         answer.reload
       }.to change(answer, :votes_count).by(-1)
+    end
+
+    it 'vote \'like\' for question will increase its rating' do
+      sign_in user
+      expect {
+        vote_like_for_question
+        question.reload
+      }.to change(question, :votes_count).by(1)
+    end
+
+    it 'vote \'dislike\' for question will decrease its rating' do
+      sign_in user
+      expect {
+        vote_dislike_for_question
+        question.reload
+      }.to change(question, :votes_count).by(-1)
     end
   end
 end
