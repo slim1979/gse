7f431bd2f850fb6a771e4310b0576e33bad9f8ae
slim1979 <chinezan@yandex.ru>
2017-12-04 00:54:35 +0300

    respond json when vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,6 +6,6 @@ class VotesController < ApplicationController
     # finding the object by id and vote for object by current user
     @object = vote_for.find(params["#{vote_for.name.underscore}_id"])
     @object.vote(current_user, params[:votes_value].to_i)
-    render partial: 'votes/votes'
+    render json: { object: @object, object_type: @object.class.to_s }
   end
 end
