194bf53b0aeb0cc57d0ddced831e5d0e158457e4
slim1979 <chinezan@yandex.ru>
2017-12-08 00:55:58 +0300

    passed

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  let(:user2)           { create(:user) }
-  let(:question)        { create(:question, user: @user) }
-  let(:answer)          { create(:answer, user: @user, question: question) }
-  let(:answer_attach)   { create(:attach, attachable: answer) }
+  let(:user2)         { create(:user) }
+  let(:question)      { create(:question, user: @user) }
+  let(:answer)        { create(:answer, user: @user, question: question) }
+  let(:answer_attach) { create(:attach, attachable: answer) }
 
   sign_in_user
 
@@ -49,8 +49,8 @@ RSpec.describe AnswersController, type: :controller do
         answer.reload
         expect(response.status).to eq 200
         parsed_body = JSON.parse(response.body)
-        expect(parsed_body['id']).to eq answer.id
-        expect(parsed_body['body']).to eq answer.body
+        expect(parsed_body['answer']['id']).to eq answer.id
+        expect(parsed_body['answer']['body']).to eq answer.body
       end
     end
     context 'Answer by someone else' do
@@ -107,7 +107,13 @@ RSpec.describe AnswersController, type: :controller do
       end
       it 'will render destroy template' do
         delete :destroy, params: { id: answer }, format: :js
-        expect(response).to render_template :destroy
+        parsed_body = JSON.parse(response.body)
+        answer_as_json = answer.as_json
+        parsed_body['created_at'] = parsed_body['created_at'].to_time.localtime.strftime('%d/%m/%Y, %T')
+        parsed_body['updated_at'] = parsed_body['updated_at'].to_time.localtime.strftime('%d/%m/%Y, %T')
+        answer_as_json['created_at'] = answer_as_json['created_at'].to_time.localtime.strftime('%d/%m/%Y, %T')
+        answer_as_json['updated_at'] = answer_as_json['updated_at'].to_time.localtime.strftime('%d/%m/%Y, %T')
+        expect(parsed_body).to eq answer_as_json
       end
     end
     context 'Answer by other author' do
@@ -121,7 +127,8 @@ RSpec.describe AnswersController, type: :controller do
         sign_out @user
         sign_in user2
         delete :destroy, params: { id: answer }, format: :js
-        expect(response).to render_template :destroy
+        response_body = JSON.parse(response.body)
+        expect(response_body['alert']).to eq 'У Вас недостаточно прав на это действие. Обратитесь в техподдержку.'
       end
     end
     context 'Answer by unauthenticated user' do
