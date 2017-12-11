5a767fcb299ef96d20de82def12cdd17f3385784
slim1979 <chinezan@yandex.ru>
2017-12-11 04:15:40 +0300

    passed

--- spec/acceptance/questions/add_files_to_question_spec.rb
+++ spec/acceptance/questions/add_files_to_question_spec.rb
@@ -8,17 +8,20 @@ feature 'Add files to question', %q{
 
   given(:user) { create(:user) }
 
-  background do
-    sign_in user
-    visit new_question_path
+  before do
   end
 
   scenario 'User adds files when asked question', js: true do
-    fill_in 'Заголовок', with: 'Test question'
-    fill_in 'Содержание', with: 'text text'
+    sign_in user
+    visit questions_path
 
-    3.times do
-      click_on 'Add an attach'
+    click_on 'Задать вопрос'
+
+    fill_in 'Заголовок', with: 'Question title'
+    fill_in 'Содержание', with: 'Question text'
+
+    4.times do
+      click_on 'Прикрепить файлы'
     end
 
     inputs = all('input[type="file"]')
@@ -27,7 +30,9 @@ feature 'Add files to question', %q{
     inputs[2].set("#{Rails.root}/spec/acceptance/questions/edit_question_spec.rb")
     inputs[3].set("#{Rails.root}/spec/acceptance/questions/questions_list_spec.rb")
 
-    click_on 'Create'
+    click_on 'Создать'
+
+    click_on 'Question title'
 
     expect(page).to have_link 'create_question_spec.rb', href: '/uploads/attach/file/1/create_question_spec.rb'
     expect(page).to have_link 'destroy_questions_spec.rb', href: '/uploads/attach/file/2/destroy_questions_spec.rb'
