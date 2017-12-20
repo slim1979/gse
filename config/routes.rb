97736c6d8eff665d3790c45acdd89dc53b2f9008
slim1979 <chinezan@yandex.ru>
2017-12-20 14:42:36 +0300

    action cable mount

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     end
   end
   root 'questions#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount ActionCable.server => '/cable'
 end
