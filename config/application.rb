b64367a53017fb333298c6b1597224a88dd18cb9
slim1979 <chinezan@yandex.ru>
2017-12-21 22:43:28 +0300

    irb instead of pry

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ module Gse
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ru
-
+    console do
+      require 'irb'
+      config.console = IRB
+    end
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
