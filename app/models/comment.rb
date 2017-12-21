bd17f0837fbd9f6ec3189ac5248f68484b86e9b0
slim1979 <chinezan@yandex.ru>
2017-12-21 23:20:50 +0300

    init

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,21 @@
 class Comment < ApplicationRecord
-  belongs_to :answer
+  belongs_to :user
+  belongs_to :commented, polymorphic: true
+
+  validates :body, :user, presence: true
+
+  after_create :publish_comment
+
+  private
+
+  def publish_comment
+    return if errors.any?
+    ActionCable.server.broadcast(
+      'comments',
+      ApplicationController.render(
+        partial: 'comments/new_comment',
+        locals: { comment: self }
+      )
+    )
+  end
 end
