6cc24b8d5627b3f73dc7103d90a5c0a320528d70
slim1979 <chinezan@yandex.ru>
2018-01-18 17:23:37 +0300

    responders deleted

--- app/controllers/attaches_controller.rb
+++ app/controllers/attaches_controller.rb
@@ -2,11 +2,8 @@ class AttachesController < ApplicationController
   before_action :authenticate_user!
   before_action :load_attach
 
-  respond_to :json
-
   def destroy
     @attach.destroy if current_user.author_of?(@attach.attachable)
-    respond_with @attach
   end
 
   private
