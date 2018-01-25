15bdc2e1ffb2072cfa080ad0d8259c95512b888c
slim1979 <chinezan@yandex.ru>
2018-01-26 01:32:24 +0300

    factory_girl to factory_bot

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Gse
                        routing_specs: false,
                        request_specs: false,
                        controller_specs: true
-      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
