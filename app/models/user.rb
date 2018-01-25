2980eeb03a1c6922b6774c897f3f11c0b41279e2
slim1979 <chinezan@yandex.ru>
2018-01-26 01:32:11 +0300

    omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+  has_many :authorizations, dependent: :destroy
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :votes, dependent: :destroy
@@ -11,5 +11,14 @@ class User < ApplicationRecord
 
   def author_of?(resource)
     resource.user_id == id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
   end
 end
