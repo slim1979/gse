e69f10f24254a76d2c6b0b0075314d60b0b7a2bb
slim1979 <chinezan@yandex.ru>
2018-01-30 20:59:32 +0300

    structure changed, #first_or_create_auth added, 11/11 passed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125211830) do
+ActiveRecord::Schema.define(version: 20180129234453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,12 @@ ActiveRecord::Schema.define(version: 20180125211830) do
     t.datetime "updated_at", null: false
     t.index ["commented_id", "commented_type"], name: "index_comments_on_commented_id_and_commented_type"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
