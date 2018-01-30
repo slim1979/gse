9c4595a56b62e0fbf6c7471bbc45e6c0be680760
slim1979 <chinezan@yandex.ru>
2018-01-30 23:17:44 +0300

    action on mail confirmation

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -1,5 +1,6 @@
 class AuthorizationsController < ApplicationController
   def confirm_email
-    @id = params[:id]
+    authorization = Authorization.find params[:id]
+    authorization.update!(email: params[:email], confirmed_at: Time.now)
   end
 end
