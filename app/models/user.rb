a1bfb25e87e2333072eaf300c5636906516236ad
slim1979 <chinezan@yandex.ru>
2018-01-30 23:17:29 +0300

    new_authorization, authorization_confirmed?, send_confirmation_email added

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,16 +16,29 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
-    return authorization.user if authorization
-
-    # new_autorization(auth)
+    authorization.user if authorization
   end
 
-  def self.new_autorization(auth)
+  def self.both_user_and_authorization_create(email, auth)
     password = Devise.friendly_token[0, 20]
-    email = [Devise.friendly_token[0, 8], '@temporary_mail.ru'].join
     user = User.create(email: email, password: password, password_confirmation: password)
-    user.authorizations.create(provider: auth.provider, uid: auth.uid)
+    user.new_autorization(auth)
     user
+  end
+
+  def new_authorization(auth)
+    new_authorization = self.authorizations.create(provider: auth['provider'], uid: auth['uid'])
+    send_confirmation_email(self, new_authorization)
+  end
+
+  def authorization_confirmed?(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    authorization.email if authorization
+  end
+
+  private
+
+  def send_confirmation_email(user, authorization)
+    AuthorizationMailer.confirm_email(user, authorization.id).deliver_now
   end
 end
