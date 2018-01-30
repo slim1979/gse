eea67eafc13271fa844ef245c3de1ef91f0ffdc3
slim1979 <chinezan@yandex.ru>
2018-01-30 23:16:29 +0300

    improved

--- app/mailers/authorization_mailer.rb
+++ app/mailers/authorization_mailer.rb
@@ -1,6 +1,7 @@
 class AuthorizationMailer < ApplicationMailer
   def confirm_email(user, authorization)
     @user = user
+    @email = user.email
     @authorization = authorization
 
     mail(to: user.email, subject: 'Подтвердите адрес почты')
