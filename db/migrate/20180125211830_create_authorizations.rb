ab62583cf8e65a321945e441b795d265295385f6
slim1979 <chinezan@yandex.ru>
2018-01-30 23:14:51 +0300

    email and confirmation date added to authorization

--- db/migrate/20180125211830_create_authorizations.rb
+++ db/migrate/20180125211830_create_authorizations.rb
@@ -4,6 +4,8 @@ class CreateAuthorizations < ActiveRecord::Migration[5.1]
       t.references :user, foreign_key: true, index: true
       t.string :provider
       t.string :uid
+      t.string :email
+      t.string :confirmed_at, default: nil
 
       t.timestamps
     end
