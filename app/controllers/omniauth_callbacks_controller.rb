e5688647e554c93f6aaf2dffad5ab4cdf9c24eed
slim1979 <chinezan@yandex.ru>
2018-01-30 23:18:40 +0300

    request_email, check_for_auth_confirmed?, email_params

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,37 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    # render json: request.env['omniauth.auth']
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'facebook') if is_navigational_format?
+  def request_email
+    @user = User.where(email: email_params[:body]).first
+    @session = session['device.facebook_data']
+    if @user
+      @user.new_authorization(@session)
+    else
+      @user = User.both_user_and_authorization_create(email_params[:body], @session)
     end
+  end
+
+  def facebook
+    auth = request.env['omniauth.auth']
+    user = User.find_for_oauth(auth)
+    if user
+      check_for_is_authorization_confirmed?(user, auth)
+    else
+      session['device.facebook_data'] = auth
+      render 'omniauth_callbacks/get_email'
+    end
+  end
+
+  private
+
+  def check_for_is_authorization_confirmed?(user, auth)
+    if user.authorization_confirmed?(auth)
+      sign_in_and_redirect user, event: :authentication
+      set_flash_message(:notice, :success, kind: 'лицокнига') if is_navigational_format?
+    else
+      render 'omniauth_callbacks/resend_confirmation_email'
+    end
+  end
+
+  def email_params
+    params.require(:email).permit(:body)
   end
 end
