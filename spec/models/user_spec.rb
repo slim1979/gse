29742d1389e74c105cd172071ba3fcb09f534688
slim1979 <chinezan@yandex.ru>
2018-01-31 19:51:26 +0300

    9/9 passed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,36 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers).dependent(:destroy) }
+  it { should have_many(:authorizations).dependent(:destroy) }
+  # . if class method, # if instance method
+  describe '.find_for_oauth' do
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '12345')}
+
+    context 'user already has authorization' do
+      it 'returns the user' do
+        user.authorizations.create(provider: 'facebook', uid: '12345')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+
+    context 'user has no authorization' do
+      context 'user already exists' do
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '12345', info: { email: user.email }) }
+        it 'does not create user' do
+          expect{ User.find_for_oauth(auth) }.to_not change(User, :count)
+        end
+        it 'creates authorization for user' do
+          expect{ user.first_or_create_authorization(auth) }.to change(user.authorizations, :count).by(1)
+        end
+        it 'creates authorization with provider and uid' do
+          user.first_or_create_authorization(auth)
+          authorization = user.authorizations.first
+
+          expect(authorization.provider).to eq auth.provider
+          expect(authorization.uid).to eq auth.uid
+        end
+      end
+    end
+  end
 end
