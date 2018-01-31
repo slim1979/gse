c1368d4bc2154a6d046a712cc77689abb3c412bb
slim1979 <chinezan@yandex.ru>
2018-01-31 19:52:47 +0300

    redirect after confirmation

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -2,5 +2,6 @@ class AuthorizationsController < ApplicationController
   def confirm_email
     authorization = Authorization.find params[:id]
     authorization.update!(email: params[:email], confirmed_at: Time.now)
+    redirect_to new_user_session_path
   end
 end
