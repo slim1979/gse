81fa94fcfb8512a49a5e16d5a21ddbec1e94f010
slim1979 <chinezan@yandex.ru>
2018-01-31 19:52:58 +0300

    redudant removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   devise_scope :user do
-    get 'emails',  to: 'omniauth_callbacks#request_email'
     post 'emails', to: 'omniauth_callbacks#request_email'
   end
   resources :attaches
