96d2565ab97d3c3a02acabaf6e98d0b26bfa604c
slim1979 <chinezan@yandex.ru>
2018-01-31 22:36:02 +0300

    capybara-email

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'capybara/rspec'
+require 'capybara/email/rspec'
 require 'shoulda/matchers'
 require 'pry'
 ENV['RAILS_ENV'] ||= 'test'
