ddeecc7f1cd4a6c5c80074888ef1b29df5df4962
slim1979 <chinezan@yandex.ru>
2018-02-05 16:26:49 +0300

    returned user.skip_confirmation

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -1,6 +1,8 @@
 class AuthorizationsController < ApplicationController
   def confirm_email
     authorization = Authorization.find params[:id]
+    user = authorization.user
+    user.update!(confirmed_at: Time.now)
     authorization.update!(email: params[:email], confirmed_at: Time.now)
     redirect_to new_user_session_path
   end
