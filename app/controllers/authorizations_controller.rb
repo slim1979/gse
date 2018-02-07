6de1c8178608e853428c59eec9d76dbf9a1fb58f
slim1979 <chinezan@yandex.ru>
2018-02-07 22:32:14 +0300

    sign in after confirmation, flash notice

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -4,6 +4,7 @@ class AuthorizationsController < ApplicationController
     user = authorization.user
     user.update!(confirmed_at: Time.now)
     authorization.update!(email: params[:email], confirmed_at: Time.now)
-    redirect_to new_user_session_path
+    flash[:notice] = "Рады приветствовать Вас на нашем сайте! Вход осуществлен с помощью #{authorization.provider.capitalize}"
+    sign_in_and_redirect user, event: :authentication
   end
 end
