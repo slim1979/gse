72e1915d3836662f99102f93521fb2029a2a708d
slim1979 <chinezan@yandex.ru>
2018-02-09 23:09:45 +0300

    fully restructured, improved. redudant deleted

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,64 +1,45 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def request_email
-    email = email_params[:address]
-    this_session_data = session["device.#{social_network}_data"]
-    email_received(this_session_data, email)
-    redirect_to new_user_session_path
-  end
-
   def facebook
-    auth_data = session['device.facebook_data'] = request.env['omniauth.auth']
-    email = request.env['omniauth.auth']['info']['email']
-    email_received(auth_data, email) if email
-    email_missed unless email
+    authorize_me_with_social_network
   end
 
   def twitter
-    auth_data = session['device.twitter_data'] = request.env['omniauth.auth']
-    email = request.env['omniauth.auth']['info']['email']
-    email_received(auth_data, email) if email
-    email_missed unless email
+    authorize_me_with_social_network
+  end
+
+  def request_email
+    authorization_with(session['devise.omniauth_data'], email_params[:address])
   end
 
   private
 
-  def email_received(auth, email)
-    user = User.where(email: email).first
-    authorization?(auth, user) if user
-    User.both_user_and_authorization_create(email, auth)
+  def authorize_me_with_social_network
+    auth = session['devise.omniauth_data'] = request.env['omniauth.auth'].except('extra')
+    email = auth['info']['email']
+    authorization_with(auth, email)
   end
 
-  def email_missed
-    auth = session["device.#{social_network}_data"]
-    user = User.find_for_oauth(auth)
+  def authorization_with(auth, email)
+    user = User.find_for_oauth(auth, email)
     if user
-      check_for_is_authorization_confirmed?(user, auth)
+      check_for_is_authorization_confirmed?(auth, user)
     else
-      ask_for_email
+      ask_for_email(auth)
     end
   end
 
-  def ask_for_email
-    render 'omniauth_callbacks/get_email', locals: { social: social_network }
+  def ask_for_email(auth)
+    render 'omniauth_callbacks/get_email', locals: { social: auth['provider'].capitalize }
   end
 
-  def authorization?(auth, user)
-    authorization = user.authorizations.where(provider: auth.provider, uid: auth.uid)
-    check_for_is_authorization_confirmed?(user, authorization) if authorization
-    user.first_or_create_authorization(session["device.#{social_network}_data"]) unless authorization
-  end
-
-  def check_for_is_authorization_confirmed?(user, auth)
+  def check_for_is_authorization_confirmed?(auth, user)
     if user.authorization_confirmed?(auth)
       sign_in_and_redirect user, event: :authentication
-      set_flash_message(:notice, :success, kind: social_network) if is_navigational_format?
+      set_flash_message(:notice, :success, kind: auth['provider'].capitalize) if is_navigational_format?
     else
-      render 'omniauth_callbacks/resend_confirmation_email'
+      user.first_or_create_authorization(auth)
+      render 'omniauth_callbacks/resend_confirmation_email', locals: { email: user.email }
     end
-  end
-
-  def social_network
-    ['facebook', 'twitter'].detect { |social| session["device.#{social}_data"]}
   end
 
   def email_params
