b442d97d31333616ef519c8672877ae8f7dc6208
slim1979 <chinezan@yandex.ru>
2018-02-10 13:26:44 +0300

    business-logic replaced from controller to model

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -1,10 +1,7 @@
 class AuthorizationsController < ApplicationController
   def confirm_email
-    authorization = Authorization.find params[:id]
-    user = authorization.user
-    user.update!(confirmed_at: Time.now)
-    authorization.update!(email: params[:email], confirmed_at: Time.now)
+    authorization = Authorization.confirm_me_with(params[:id], params[:email])
     flash[:notice] = "Рады приветствовать Вас на нашем сайте! Вход осуществлен с помощью #{authorization.provider.capitalize}"
-    sign_in_and_redirect user, event: :authentication
+    sign_in_and_redirect authorization.user, event: :authentication
   end
 end
