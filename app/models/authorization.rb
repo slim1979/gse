014cc14e0d1c414e32ca39411e984eca5eeec611
slim1979 <chinezan@yandex.ru>
2018-02-10 13:26:44 +0300

    business-logic replaced from controller to model

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -2,4 +2,13 @@ class Authorization < ApplicationRecord
   belongs_to :user
 
   validates :uid, uniqueness: true
+
+
+  def self.confirm_me_with(id, email)
+    authorization = Authorization.find id
+    user = authorization.user
+    user.update!(confirmed_at: Time.now)
+    authorization.update!(email: email, confirmed_at: Time.now)
+    authorization
+  end
 end
