ccd990201665000aed4c02f9cf3c00b024299568
slim1979 <chinezan@yandex.ru>
2018-02-17 02:54:16 +0300

    admin, user and guest abilities added

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,30 @@
 class Ability
   include CanCan::Ability
 
+  attr_reader :user
+
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    user ||= User.new # guest user (not logged in)
-    if user.admin?
-      can :manage, :all
+    @user = user
+    if user
+      user.admin? ? admin_abilities : user_abilities
     else
-      can :read, :all
+      guest_abilities
     end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+  end
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def user_abilities
+    guest_abilities
+    can :create, [Question, Answer, Comment]
+    can %i[update destroy], [Question, Answer, Comment], user: user
+    can :assign_best, Answer, question: { user: user }
+    can :manage, Attach, attachable: { user: user }
+  end
+
+  def admin_abilities
+    can :manage, :all
   end
 end
