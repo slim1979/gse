e202ccf8ef781ce6030814b42b55be95c4a519fa
slim1979 <chinezan@yandex.ru>
2018-02-21 21:51:16 +0300

    quest can confirm email, user can create Vote among others

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,12 +13,13 @@ class Ability
   end
 
   def guest_abilities
+    can :confirm_email, :all
     can :read, :all
   end
 
   def user_abilities
     guest_abilities
-    can :create, [Question, Answer, Comment]
+    can :create, [Question, Answer, Comment, Vote]
     can %i[update destroy], [Question, Answer, Comment], user: user
     can :assign_best, Answer, question: { user: user }
     can :manage, Attach, attachable: { user: user }
