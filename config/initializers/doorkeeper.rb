5d7535e2f72e1ad9a95090c06fff345061db6b53
slim1979 <chinezan@yandex.ru>
2018-02-27 21:31:54 +0300

    current user removed

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -4,7 +4,7 @@ Doorkeeper.configure do
 
   # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    current_user ||= warden.authenticate!(scope: :user)
+    warden.authenticate!(scope: :user)
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
