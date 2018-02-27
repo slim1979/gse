d63d08bcdf317751d69f844df10815a3f007ed21
slim1979 <chinezan@yandex.ru>
2018-02-27 21:19:23 +0300

    redudant change of content_type removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
+      format.json { head :forbidden }
       format.html { redirect_to root_url, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
+      format.js   { head :forbidden }
     end
   end
 
