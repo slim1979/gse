9f34bc925693b6843ef8de47f1dd27d8ba60bd72
slim1979 <chinezan@yandex.ru>
2018-02-27 20:31:36 +0300

    syntax correction

--- spec/factories/oauth_applications.rb
+++ spec/factories/oauth_applications.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :oauth_application, class: ::Doorkeeper::Application do
+  factory :oauth_application, class: Doorkeeper::Application do
     name 'Test'
     redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
     uid '12345678'
