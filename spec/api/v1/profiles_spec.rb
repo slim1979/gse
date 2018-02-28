8f9183886862e10d56807980b004653b9948efd3
slim1979 <chinezan@yandex.ru>
2018-02-28 21:13:58 +0300

    7/7 passed, refactored

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -16,11 +16,24 @@ describe 'Profile API' do
 
     context 'authorized' do
       let(:me) { create(:user) }
-      let(:access_token) { create(:access_token) }
-
-      it 'returns status 200' do
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+      before do
         get '/api/v1/profiles/me', params: { format: :json, access_token: access_token.token }
+      end
+      it 'returns status 200' do
         expect(response.status).to eq 200
+      end
+
+      %w[id email created_at updated_at admin].each do |kind|
+        it "contain #{kind}" do
+          expect(response.body).to be_json_eql(me.send(kind.to_sym).to_json).at_path(kind)
+        end
+      end
+
+      %w[password encrypted_password].each do |kind|
+        it "does not contain #{kind}" do
+          expect(response.body).to_not have_json_path(kind)
+        end
       end
     end
   end
