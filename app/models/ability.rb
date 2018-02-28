b0e7f246e915d5c8270d98990c1f5311ad450e17
slim1979 <chinezan@yandex.ru>
2018-02-28 21:11:56 +0300

    refactoring

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,9 +20,7 @@ class Ability
   def user_abilities
     guest_abilities
     can :create, [Question, Answer, Comment]
-    can :vote, [Answer, Question] do |subject|
-      subject.user_id != user.id
-    end
+    can :vote, [Answer, Question] { |subject| subject.user_id != user.id }
     can %i[update destroy], [Question, Answer, Comment], user_id: user.id
     can :assign_best, Answer, question: { user_id: user.id }
     can :manage, Attach, attachable: { user_id: user.id }
