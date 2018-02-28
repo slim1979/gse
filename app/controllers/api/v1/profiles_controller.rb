c84ce3e4d22530d7d1834a4903797340fbcd9149
slim1979 <chinezan@yandex.ru>
2018-02-28 16:57:05 +0300

    respond_to json, authorize load profile, current ability

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,20 @@
 class Api::V1::ProfilesController < ApplicationController
   before_action :doorkeeper_authorize!
 
+  respond_to :json
+
   def me
-    render nothing: true
+    authorize! :load, :profile
+    respond_with current_user
+  end
+
+  protected
+
+  def current_resource_owner
+    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_resource_owner)
   end
 end
