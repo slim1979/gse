078623679813af2d0c8dae194a60f3e7b4408fae
slim1979 <chinezan@yandex.ru>
2018-02-28 21:13:08 +0300

    from BaseController now, current_resource_owner

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,18 +1,10 @@
-class Api::V1::ProfilesController < ApplicationController
-  before_action :doorkeeper_authorize!
-
-  respond_to :json
-
+class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     authorize! :load, :profile
-    respond_with current_user
+    respond_with current_resource_owner
   end
 
   protected
-
-  def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
 
   def current_ability
     @current_ability ||= Ability.new(current_resource_owner)
