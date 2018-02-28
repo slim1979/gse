661fc85858efa55006d3ce820e228b961e944ad1
slim1979 <chinezan@yandex.ru>
2018-03-01 01:29:59 +0300

    GET index passed, tests on 'unauthorized' moved to separate describe

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -2,18 +2,6 @@ require 'rails_helper'
 
 describe 'Profile API' do
   describe 'GET /me' do
-    context 'unauthorized' do
-      it 'return 401 status if there is no access token' do
-        get '/api/v1/profiles/me', params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'return 401 status if access token is invalid' do
-        get '/api/v1/profiles/me', params: { format: :json, access_token: '1213131' }
-        expect(response.status).to eq 401
-      end
-    end
-
     context 'authorized' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
@@ -34,6 +22,39 @@ describe 'Profile API' do
         it "does not contain #{kind}" do
           expect(response.body).to_not have_json_path(kind)
         end
+      end
+    end
+  end
+
+  describe 'GET index' do
+    context 'authorized' do
+      let!(:users) { create_list(:user, 5) }
+      let(:access_token) { create(:access_token, resource_owner_id: users[0].id) }
+
+      before do
+        get '/api/v1/profiles', params: { format: :json, access_token: access_token.token }
+      end
+
+      it 'return status 200' do
+        expect(response).to be_success
+      end
+
+      it 'contain users list, except current user' do
+        expect(response.body).to be_json_eql(users[1..4].to_json).at_path('/')
+      end
+    end
+  end
+
+  describe 'unauthorized' do
+    %w[/me /].each do |path|
+      it "return 401 status if there is no access token on #{path}" do
+        get "/api/v1/profiles#{path}", params: { format: :json }
+        expect(response.status).to eq 401
+      end
+
+      it "return 401 status if access token is invalid on #{path}" do
+        get "/api/v1/profiles#{path}", params: { format: :json, access_token: '1213131' }
+        expect(response.status).to eq 401
       end
     end
   end
