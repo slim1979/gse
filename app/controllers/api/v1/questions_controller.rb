a77700d97e9f5ceeaa9254d1a328cf524aa7381a
slim1979 <chinezan@yandex.ru>
2018-03-01 19:03:10 +0300

    action index added

---
+++ app/controllers/api/v1/questions_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::QuestionsController < Api::V1::BaseController
+
+  authorize_resource
+
+  def index
+    @questions = Question.all
+    respond_with(@questions.to_json(include: :answers))
+  end
+end
