60069d8cd0b8e10b3039f9dfccecdf46b089baad
slim1979 <chinezan@yandex.ru>
2018-03-02 19:52:44 +0300

    removed comments and attaches

--- app/serializers/answer_serializer.rb
+++ app/serializers/answer_serializer.rb
@@ -1,6 +1,3 @@
 class AnswerSerializer < ActiveModel::Serializer
   attributes :id, :body, :question_id, :created_at, :updated_at, :user_id, :best_answer, :votes_count
-
-  has_many :comments
-  has_many :attaches
 end
