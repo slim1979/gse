3559c7f55fa12eb06d54abcab1d949fcfcb3edf3
slim1979 <chinezan@yandex.ru>
2018-03-04 01:42:15 +0300

    refactored

--- spec/support/shared/create_with_invalid_attributes.rb
+++ spec/support/shared/create_with_invalid_attributes.rb
@@ -1,12 +1,14 @@
 shared_examples 'Create with invalid attributes' do
-  before { load_params }
+  before do
+    load_params
+    invalid_post_create
+  end
 
   it 'does not save the new question to DB' do
-    expect { @request_params[:request] }.to_not change(@request_params[:object], :count)
+    expect { invalid_post_create }.to_not change(@shared_params[:object], :count)
   end
 
   it 'renders template' do
-    @request_params[:request]
-    expect(response).to render_template @request_params[:render]
+    expect(response).to render_template @shared_params[:render]
   end
 end
