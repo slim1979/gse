20926311e29d354b66311bbe1f206abe6508102a
slim1979 <chinezan@yandex.ru>
2018-03-05 14:10:08 +0300

    improved to use shared_examples 'Add files'

--- spec/acceptance/answers/add_files_to_answer_spec.rb
+++ spec/acceptance/answers/add_files_to_answer_spec.rb
@@ -9,30 +9,29 @@ feature 'Add files to answer', %q{
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
-  before do
-    sign_in user
-    visit question_path(question)
-  end
-
-  scenario 'User adds files when asked question', js: true do
-    fill_in 'Содержание', with: 'text text'
-    3.times do
-      click_on 'Прикрепить файлы'
-    end
-    inputs = all('input[type="file"]')
-    inputs[0].set("#{Rails.root}/spec/acceptance/questions/create_question_spec.rb")
-    inputs[1].set("#{Rails.root}/spec/acceptance/questions/destroy_questions_spec.rb")
-    inputs[2].set("#{Rails.root}/spec/acceptance/questions/edit_question_spec.rb")
-    inputs[3].set("#{Rails.root}/spec/acceptance/questions/questions_list_spec.rb")
-
-    click_on 'Answer the question'
-    wait_for_ajax
-    expect(page).to have_css '.answer'
-    within '.answer' do
-      expect(page).to have_link 'create_question_spec.rb'
-      expect(page).to have_link 'destroy_questions_spec.rb'
-      expect(page).to have_link 'edit_question_spec.rb'
-      expect(page).to have_link 'questions_list_spec.rb'
-    end
-  end
+  it_behaves_like 'Add files', 'answer'
+  # scenario 'User adds files when answered the question', js: true do
+  #   sign_in user
+  #   visit question_path(question)
+  #
+  #   fill_in 'Содержание', with: 'text text'
+  #   3.times do
+  #     click_on 'Прикрепить файлы'
+  #   end
+  #   inputs = all('input[type="file"]')
+  #   inputs[0].set("#{Rails.root}/spec/acceptance/questions/create_question_spec.rb")
+  #   inputs[1].set("#{Rails.root}/spec/acceptance/questions/destroy_questions_spec.rb")
+  #   inputs[2].set("#{Rails.root}/spec/acceptance/questions/edit_question_spec.rb")
+  #   inputs[3].set("#{Rails.root}/spec/acceptance/questions/questions_list_spec.rb")
+  #
+  #   click_on 'Answer the question'
+  #   wait_for_ajax
+  #   expect(page).to have_css '.answer'
+  #   within '.answer' do
+  #     expect(page).to have_link 'create_question_spec.rb'
+  #     expect(page).to have_link 'destroy_questions_spec.rb'
+  #     expect(page).to have_link 'edit_question_spec.rb'
+  #     expect(page).to have_link 'questions_list_spec.rb'
+  #   end
+  # end
 end
