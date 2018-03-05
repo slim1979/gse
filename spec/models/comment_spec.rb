36ba028252496800d7ab351b2510ff1327ac4e2f
slim1979 <chinezan@yandex.ru>
2018-03-05 13:06:55 +0300

    redudant removed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it { should belong_to :user }
-  it { should belong_to :user }
   it { should validate_presence_of :body }
   it { should validate_presence_of :user }
 end
