ff16fe614d3a4e72ece249ed61e633a48a868318
slim1979 <chinezan@yandex.ru>
2018-03-26 13:53:41 +0300

    improved to use shared_examples 'Vote for' object

--- spec/acceptance/votes/vote_for_question_spec.rb
+++ spec/acceptance/votes/vote_for_question_spec.rb
@@ -13,6 +13,7 @@ feature 'Votes for question', %q{
   describe 'Authenticated user' do
     let(:like) { find('a.like').click }
     let(:dislike) { find('a.dislike').click }
+
     context 'author of question does not can vote for question' do
       scenario 'do not see the \'vote for question\' buttons' do
         sign_in author_of_question
@@ -29,49 +30,19 @@ feature 'Votes for question', %q{
       before do
         sign_in some_other_user
         visit question_path(question)
-        @current_question_votes_count = question.votes_count
+        @votes_count = question.votes_count
       end
 
-      scenario 'vote for the question - like - for the first time', js: true do
-        within '.question' do
-          like
-          within ".question_votes_count_#{question.id}" do
-            expect(page).to have_content @current_question_votes_count + 1
-          end
-        end
-      end
+      it_behaves_like 'Vote for', 'question'
 
-      scenario 'vote for the question - dislike - for the first time', js: true do
-        within '.question' do
-          dislike
-          within ".question_votes_count_#{question.id}" do
-            expect(page).to have_content @current_question_votes_count - 1
-          end
-        end
-      end
-
-      scenario 'vote for the question - like - the second and subsequent times, by same user will not increase the answer rating', js: true do
-        within '.question' do
-          3.times { like }
-          within ".question_votes_count_#{question.id}" do
-            expect(page).to have_content 1
-          end
-        end
-      end
-
-      scenario 'vote for the question - dislike - the second and subsequent times will not decrease the answer rating', js: true do
-        within '.question' do
-          3.times { dislike }
-          within ".question_votes_count_#{question.id}" do
-            expect(page).to have_content(-1)
-          end
-        end
+      def load_params
+        @shared_params = { object: question }
       end
     end
   end
 
   describe 'Unauthenticated user' do
-    scenario 'tries to vote for answer' do
+    scenario 'tries to vote for question' do
       visit question_path(question)
 
       expect(page).to_not have_link 'like'
