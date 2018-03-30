d74d0e63141b33a4980678249261a1690171857c
slim1979 <chinezan@yandex.ru>
2018-03-30 21:35:03 +0300

    user can create Subscription

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -19,7 +19,7 @@ class Ability
 
   def user_abilities
     guest_abilities
-    can :create, [Question, Answer, Comment]
+    can :create, [Question, Answer, Comment, Subscription]
     can :vote, [Answer, Question] { |subject| subject.user_id != user.id }
     can %i[update destroy], [Question, Answer, Comment], user_id: user.id
     can :assign_best, Answer, question: { user_id: user.id }
