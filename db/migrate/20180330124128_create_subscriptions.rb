933d6034287eb9745b261ca4d052d68b96a0fbf1
slim1979 <chinezan@yandex.ru>
2018-03-30 21:34:45 +0300

    migration

--- db/migrate/20180330124128_create_subscriptions.rb
+++ db/migrate/20180330124128_create_subscriptions.rb
@@ -1,6 +1,8 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.1]
   def change
     create_table :subscriptions do |t|
+      t.belongs_to :user
+      t.belongs_to :question
 
       t.timestamps
     end
