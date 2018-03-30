bfd5f94b5ea1237cf7d9aaded028a8c098e8740a
slim1979 <chinezan@yandex.ru>
2018-03-30 21:38:04 +0300

    post create green

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionsController, type: :controller do
+  let(:user)             { create(:user) }
+  let(:question)         { create(:question, user: user) }
 
+  sign_in_user
+
+  describe 'POST #create' do
+    it_behaves_like 'POST #create', 'subscription'
+
+    def valid_post_create
+      post :create, params: { id: question, format: :js }
+    end
+
+    def invalid_post_create
+      post :create, params: { id: nil, format: :js }
+    end
+
+    def load_params
+      @shared_params = { object: Subscription, render: :create }
+    end
+  end
 end
