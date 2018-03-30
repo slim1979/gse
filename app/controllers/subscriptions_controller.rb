842f28efcc1d5d0e36b72e225bb1c360b357c7ab
slim1979 <chinezan@yandex.ru>
2018-03-30 21:36:50 +0300

    action create

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,11 @@
 class SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
+  respond_to :js
+  authorize_resource
+
+  def create
+    @subscription = Subscription.create(user: current_user, question_id: params[:id])
+    respond_with(@subscription)
+  end
 end
