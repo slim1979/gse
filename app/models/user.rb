7887d4a04265fdc84330eb0af0cba3648e9fcf0e
slim1979 <chinezan@yandex.ru>
2018-03-31 22:28:18 +0300

    self method send_daily_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,15 @@ class User < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  def self.send_daily_digest
+    questions = Question.created_yesterday.to_a
+    pluck(:email).in_groups_of(100) do |group|
+      group.compact!.each do |email|
+        DailyMailer.digest(email, questions).deliver_later
+      end
+    end
+  end
+
   def author_of?(resource)
     resource.user_id == id
   end
