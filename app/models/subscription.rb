46c33f887ca15351dc73da92b4cdafe24801be67
slim1979 <chinezan@yandex.ru>
2018-03-31 22:28:43 +0300

    send notice by email

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,6 +7,6 @@ class Subscription < ApplicationRecord
   private
 
   def send_subscription
-    SubscribeInitMailer.send_subscription_confirmation(user, question).deliver_now
+    SubscribeInitMailer.send_subscription_confirmation(user, question).deliver_later
   end
 end
