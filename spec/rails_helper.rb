f9b558918e3a9aecaa350f55935ae0c3818fe729
slim1979 <chinezan@yandex.ru>
2018-04-03 00:46:16 +0300

    sidekiq/testing, Sidekiq::Testing.fake!

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,14 +3,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'rspec/rails'
 require 'spec_helper'
 require 'capybara/rspec'
 require 'capybara/email/rspec'
 require 'shoulda/matchers'
 require 'pry'
 require 'cancan/matchers'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
