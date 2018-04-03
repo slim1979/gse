ae7c55e4c3898ff4f0d665875cba7e9eaca4f23d
slim1979 <chinezan@yandex.ru>
2018-04-03 17:10:31 +0300

    filled with mock data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'chinezan@yandex.ru', password: 'qwerty', admin: true)
+User.create(email: 'qqq@qqq.ru', password: 'qwerty')
+
+5.times do
+  User.create do |user|
+    user.email = Faker::Internet.email
+    user.password = 'qwerty'
+  end
+end
+
+15.times do
+  Question.create do |question|
+    question.title = Faker::Lorem.paragraph
+    question.body = Faker::Lorem.paragraph(2)
+    question.user = User.all.sample
+  end
+end
+
+50.times do
+  Answer.create do |answer|
+    answer.body = Faker::Lorem.paragraph(2)
+    answer.user = User.all.sample
+    answer.question = Question.all.sample
+  end
+end
