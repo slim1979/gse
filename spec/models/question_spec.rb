299991c83af819e2280f54682d48235fa51b8bb0
slim1979 <chinezan@yandex.ru>
2018-04-04 16:45:41 +0300

    after create jobs tests. passed

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Question, type: :model do
   it { should accept_nested_attributes_for :attaches }
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_many :votes }
+
+  describe 'after create' do
+    let(:user) { create(:user) }
+
+    it 'should create an subscription' do
+      question = Question.create(title: 'dsdsd', body: 'dasd0', user: user)
+      expect(Subscription.where(question: question, user: user).count).to eq 1
+    end
+
+    it 'should send an thanks email' do
+      expect(ThanksMailer).to receive(:send_thanks).and_call_original
+      Question.create(title: 'dsdsd', body: 'dasd0', user: user)
+    end
+  end
+
 end
