99d71495f4780a4928b535ca62be7d5c7a51687e
slim1979 <chinezan@yandex.ru>
2018-04-04 17:27:15 +0300

    redudant removed

--- app/mailers/subscribe_init_mailer.rb
+++
@@ -1,5 +0,0 @@
-class SubscribeInitMailer < ApplicationMailer
-  def send_subscription_notification(user, question)
-    mail(to: user.email, subject: "Вы подписались на вопрос #{question.title}")
-  end
-end
--- spec/mailers/previews/subscribe_init_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/subscribe_init
-class SubscribeInitPreview < ActionMailer::Preview
-
-end
