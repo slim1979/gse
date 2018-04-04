1cba08c0fcd72634110f81a7d770e7e67e147f0f
slim1979 <chinezan@yandex.ru>
2018-04-04 23:24:15 +0300

    filled from User.send_daily_digest

--- app/jobs/daily_digest_job.rb
+++ app/jobs/daily_digest_job.rb
@@ -1,7 +1,12 @@
 class DailyDigestJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform
+    questions = Question.created_yesterday.to_a
+    User.pluck(:email).in_groups_of(100) do |group|
+      group.compact!.each do |email|
+        DailyMailer.digest(email, questions).deliver_later
+      end
+    end
   end
 end
