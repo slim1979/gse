09079cfc04cc9fd6064b11812c6e96e93da3d5cc
slim1979 <chinezan@yandex.ru>
2018-04-04 17:51:03 +0300

    validate_uniqueness_of user_id, validate presence_of user, question

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,13 +2,6 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
+  validates :user_id, :question_id, presence: true
   validates_uniqueness_of :user_id, scope: :question_id
-
-  after_create :send_subscription
-
-  private
-
-  def send_subscription
-    SubscribeInitMailer.send_subscription_notification(user, question).deliver_later unless question.user.id == user.id
-  end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :question_id }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:question_id) }
 end
