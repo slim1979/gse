8578a4986552f6f0e3e58b301efee5a602a4e0a3
slim1979 <chinezan@yandex.ru>
2018-04-04 16:57:57 +0300

    validates uniqueness of user

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,11 +2,13 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
+  validates_uniqueness_of :user_id, scope: :question_id
+
   after_create :send_subscription
 
   private
 
   def send_subscription
-    SubscribeInitMailer.send_subscription_confirmation(user, question).deliver_later
+    SubscribeInitMailer.send_subscription_notification(user, question).deliver_later unless question.user.id == user.id
   end
 end
