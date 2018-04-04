0a9ad5519bac7794160f86e9ec57e84ffaf201b2
slim1979 <chinezan@yandex.ru>
2018-04-04 16:36:07 +0300

    init and send_notification method

---
+++ app/mailers/send_notification_to_subscribers_mailer.rb
@@ -0,0 +1,7 @@
+class SendNotificationToSubscribersMailer < ApplicationMailer
+  def send_notification(user, question, answer)
+    @answer = answer
+    @question = question
+    mail(to: user.email, subject: "Новый ответ на вопрос: #{question.title}")
+  end
+end
