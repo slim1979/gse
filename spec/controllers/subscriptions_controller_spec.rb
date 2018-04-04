7ce8786bb3046c796725e0b798518c83a9c77dab
slim1979 <chinezan@yandex.ru>
2018-04-04 21:16:12 +0300

    question saved in base

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionsController, type: :controller do
-  let(:user)             { create(:user) }
-  let(:question)         { create(:question, user: user) }
+  let(:user)      { create(:user) }
+  let!(:question) { create(:question) }
 
   sign_in_user
 
