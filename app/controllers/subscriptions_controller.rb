6a5b20ba1067e4c8165f35e57b47d90270025de1
slim1979 <chinezan@yandex.ru>
2018-04-04 17:51:52 +0300

    action destroy added

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -8,4 +8,9 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.create(user: current_user, question_id: params[:id])
     respond_with(@subscription)
   end
+
+  def destroy
+    @subscription = Subscription.where(user: current_user, question_id: params[:id]).first
+    respond_with(@subscription.destroy)
+  end
 end
