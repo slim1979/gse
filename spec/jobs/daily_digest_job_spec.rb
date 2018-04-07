118eba1cb5211b526b8ede04c91b515ef3a626a0
slim1979 <chinezan@yandex.ru>
2018-04-07 16:47:34 +0300

    passed

--- spec/jobs/daily_digest_job_spec.rb
+++ spec/jobs/daily_digest_job_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe DailyDigestJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:questions) { create_list(:question, 5, created_at: 1.days.ago) }
+
+  it 'will send daily digest to all users' do
+    User.pluck(:email).each do |email|
+      expect(DailyMailer).to receive(:digest).with(email, questions.to_a).and_call_original
+    end
+    DailyDigestJob.perform_now
+  end
 end
