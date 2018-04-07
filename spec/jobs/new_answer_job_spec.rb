405b164e2977cbfad01564408bb077d261d85d4d
slim1979 <chinezan@yandex.ru>
2018-04-07 16:23:23 +0300

    subscribers will receive notification

--- spec/jobs/new_answer_job_spec.rb
+++ spec/jobs/new_answer_job_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe NewAnswerJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question_owner) { create(:user) }
+  let(:answer_owner)   { create(:user) }
+  let(:subscribers)    { create_list(:user, 3) }
+  let(:question)       { create(:question, user: question_owner) }
+  let(:answer)         { create(:answer, question: question, user: answer_owner) }
+
+  it 'all subscribers will receive notification' do
+    subscribers.each { |subscriber| subscriber.subscriptions.create(question: question) }
+    question.subscriptions.each do |subscription|
+      expect(NewAnswerMailer).to receive(:send_notification).with(subscription.user, question, answer).and_call_original
+    end
+    NewAnswerJob.perform_now(answer)
+  end
 end
