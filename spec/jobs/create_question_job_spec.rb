cad7e73e0fface6a74b92e8ba8591d850c68935c
slim1979 <chinezan@yandex.ru>
2018-04-07 15:18:50 +0300

    2/2 passed

--- spec/jobs/create_question_job_spec.rb
+++ spec/jobs/create_question_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe CreateQuestionJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:question) { create(:question, user: user) }
+
+  it 'will create subscription' do
+    expect{ CreateQuestionJob.perform_now(user, question) }.to change(user.subscriptions, :count).by(1)
+  end
+
+  it 'will send an email' do
+    expect(ThanksMailer).to receive(:send_thanks).with(user, question).and_call_original
+    CreateQuestionJob.perform_now(user, question)
+  end
 end
