0b89c54c09289f94f512537598a358c7599be74d
slim1979 <chinezan@yandex.ru>
2018-04-11 02:16:35 +0300

    search_through nil value escaping

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,7 +4,8 @@ class SearchesController < ApplicationController
   def search_hound
     authorize! :search, :content
     search_for = params[:search_for]
-    search_through = params[:search_through].to_sym
+    search_through = params[:search_through]
+    search_through ||= :full_search
     @result = Search.make_search(search_for, search_through)
   end
 end
