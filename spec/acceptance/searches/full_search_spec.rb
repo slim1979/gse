34488504196138676759f1227084638af00fc023
slim1979 <chinezan@yandex.ru>
2018-04-10 14:17:47 +0300

    use shared examples

---
+++ spec/acceptance/searches/full_search_spec.rb
@@ -0,0 +1,63 @@
+require 'acceptance_helper'
+
+feature 'Search through site', %q{
+  In order to find needed content
+  As an authenticated user
+  I want to be able search by my conditions
+} do
+
+  let(:users)       { create_list(:user, 6) }
+  let!(:questions)  { create_list(:question, 5, user: users.sample, title: "#{('a'..'z').to_a.sample(51).join} qwerty") }
+  let!(:answers)    { create_list(:answer, 5, user: users.sample, body: "#{('a'..'z').to_a.sample(51).join} qwerty answer") }
+  let!(:comments)   { create_list(:comment, 5, commented: questions.sample, user: users.sample, body: "#{('a'..'z').to_a.sample(51).join} qwerty question_comment") }
+
+  describe 'Search by content, except users', js: true do
+    before do
+      ThinkingSphinx::Test.index
+      sign_in users.last
+      visit root_path
+      fill_in :search_for, with: 'qwerty'
+    end
+
+    it 'through all content' do
+      choose('search_through_full_search')
+      click_on 'Искать'
+
+      [questions, answers, comments].each do |objects|
+        objects.each do |object|
+          expect(page).to have_content "#{object.class == User ? object.email : object.class == Question ? object.title : object.body}"
+        end
+      end
+    end
+    describe 'questions' do
+      it_behaves_like 'search through', 'questions'
+
+      def load_params
+        questions
+      end
+    end
+    describe 'answers' do
+      it_behaves_like 'search through', 'answers'
+
+      def load_params
+        answers
+      end
+    end
+    describe 'comments' do
+      it_behaves_like 'search through', 'comments'
+
+      def load_params
+        comments
+      end
+    end
+  end
+
+  describe 'Search through users' do
+    ThinkingSphinx::Test.index
+    sign_in users.last
+    visit root_path
+    fill_in :search_for, with: 'users.last.email'
+    choose('search_through_users')
+    click_on 'Искать'
+  end
+end
