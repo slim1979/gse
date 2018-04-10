abd11cde18d9ebde8e368807517f9847a3884546
slim1979 <chinezan@yandex.ru>
2018-04-11 02:16:11 +0300

    attribute to sym

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,7 @@ class Search < ApplicationRecord
   SEARCH_TYPE = { full_search: ThinkingSphinx, questions: Question, answers: Answer, comments: Comment, users: User }.freeze
 
   def self.make_search(search_for, search_through)
-    search_through = SEARCH_TYPE[search_through]
+    search_through = SEARCH_TYPE[search_through.to_sym]
     search_through ||= ThinkingSphinx
     search_through.search ThinkingSphinx::Query.escape(search_for)
   end
