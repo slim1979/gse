bceba0536c1a51dae23dc7db7b48995da037ce32
slim1979 <chinezan@yandex.ru>
2018-04-10 12:57:32 +0300

    search_hound method added

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,8 +4,14 @@ class SearchesController < ApplicationController
   def search_hound
     authorize! :search, :content
     search_for = params[:search_for]
+<<<<<<< f0746ffb1e9ff90092e2daa888a84e1f07452508
     search_through = params[:search_through]
     search_through ||= :full_search
     @result = Search.make_search(search_for, search_through)
+=======
+    search_through = params[:search_through].to_sym
+    # debugger
+    @result = Search.new.make_search(search_for, search_through)
+>>>>>>> search_hound method added
   end
 end
