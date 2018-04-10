a520709a6de03150c03e7c750f7a81d1503e1fc0
slim1979 <chinezan@yandex.ru>
2018-04-10 13:00:59 +0300

    make_search method added

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,9 @@
 class Search < ApplicationRecord
+  SEARCH_TYPE = { full_search: ThinkingSphinx, questions: Question, answers: Answer, comments: Comment, users: User }.freeze
+
+  def make_search(search_for, search_through)
+    search_through = SEARCH_TYPE[search_through]
+    search_through ||= ThinkingSphinx
+    search_through.search search_for
+  end
 end
