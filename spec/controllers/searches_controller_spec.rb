c80e821667f65904f1bcbfcadcc0026290cb12c9
slim1979 <chinezan@yandex.ru>
2018-04-11 02:15:39 +0300

    POST search hound

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe SearchesController, type: :controller do
 
+  sign_in_user
+
+  describe 'POST #search_hound', js: true do
+    it 'will render search_hound template' do
+      post :search_hound, params: { search_for: 'ffdfd', search_through: "full_search" }
+      expect(response).to render_template :search_hound
+    end
+
+    it 'will render search_hound template' do
+      expect(Search).to receive(:make_search)
+      post :search_hound, params: { search_for: 'ffdfd', search_through: "full_search" }
+    end
+  end
 end
