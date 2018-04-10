5ec1203481faf088064a92ac3ee547adc5277ddb
slim1979 <chinezan@yandex.ru>
2018-04-10 21:49:52 +0300

    5/5 green

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'execute full search' do
+    expect(ThinkingSphinx).to receive(:search).with('request')
+    Search.make_search('request', :full_search)
+  end
+
+  ['questions', 'answers', 'comments', 'users'].each do |klass|
+    it "finds model in #{klass}" do
+      expect(klass.chop.capitalize.constantize).to receive(:search).with('request')
+      Search.make_search('request', klass.to_sym)
+    end
+  end
 end
