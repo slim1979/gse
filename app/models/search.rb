fff5762ef4ba38c30cc07daf5dc2a68be6d4e596
slim1979 <chinezan@yandex.ru>
2018-04-15 01:49:46 +0300

    rebase conflicts resolved

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,16 +1,9 @@
 class Search < ApplicationRecord
   SEARCH_TYPE = { full_search: ThinkingSphinx, questions: Question, answers: Answer, comments: Comment, users: User }.freeze
 
-<<<<<<< bceba0536c1a51dae23dc7db7b48995da037ce32
   def self.make_search(search_for, search_through)
     search_through = SEARCH_TYPE[search_through.to_sym]
     search_through ||= ThinkingSphinx
     search_through.search ThinkingSphinx::Query.escape(search_for)
-=======
-  def make_search(search_for, search_through)
-    search_through = SEARCH_TYPE[search_through]
-    search_through ||= ThinkingSphinx
-    search_through.search search_for
->>>>>>> make_search method added
   end
 end
